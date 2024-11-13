654c064397bb1d1d3d783dde3f6432b5e9890cef
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-13 15:14:20 +0000

    rf

--- modules/instructions.py
+++ modules/instructions.py
@@ -1,12 +1,29 @@
 import streamlit as st
+import streamlit as st
+
+st.markdown("""
+### Pós-graduação em Ciência da Computação - UEL
+**Projeto:** Detecção de Ataques no Tráfego de Dispositivos IoT.
+            
+**Disciplina:** 2COP507 Reconhecimento de Padrões.
+                               
+**Discente:** Wesley Candido da Silva.
+            
+**Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
+          
+**Link github:** xxx
+""")
 
 def show():
-    st.title("Instruções")
+    st.markdown("""
+    ### Pós-graduação em Ciência da Computação - UEL
+    """)     
     st.write("""
     Bem-vindo ao Classificador Random Forest! Este aplicativo permite que você:
+    
     1. Configure hiperparâmetros para o modelo Random Forest.
-    2. Treine o modelo com os dados de exemplo.
-    3. Veja o desempenho do modelo e visualize a matriz de confusão.
+    2. Treine o modelo.
+    3. Veja o desempenho do modelo.
     
     Utilize a barra lateral para navegar entre as páginas.
     """)
