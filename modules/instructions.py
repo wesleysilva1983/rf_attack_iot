73a90d138ef8ae64419b4523cbbc2e6d980d74b9
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-13 15:22:41 +0000

    rf

--- modules/instructions.py
+++ modules/instructions.py
@@ -1,24 +1,24 @@
 import streamlit as st
 import streamlit as st
 
-st.markdown("""
-### Pós-graduação em Ciência da Computação - UEL
-**Projeto:** Detecção de Ataques no Tráfego de Dispositivos IoT.
-            
-**Disciplina:** 2COP507 Reconhecimento de Padrões.
-                               
-**Discente:** Wesley Candido da Silva.
-            
-**Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
-          
-**Link github:** xxx
-""")
-
 def show():
+    st.markdown("""
+    ### Pós-graduação em Ciência da Computação - UEL
+    **Projeto:** Detecção de Ataques no Tráfego de Dispositivos IoT.
+            
+    **Disciplina:** 2COP507 Reconhecimento de Padrões.
+                               
+    **Discente:** Wesley Candido da Silva.
+            
+    **Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
+          
+    **Link github:** xxx
+    """)
+
     st.markdown("""
     ### Classificador Random Forest
                 
-    **Este aplicativo permite que você:**
+    **Funcionalidades do aplicativo:**
                 
     1. Configuração dos hiperparâmetros para o modelo RF.
     2. Treine o modelo.
