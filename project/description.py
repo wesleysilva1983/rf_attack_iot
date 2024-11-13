3b01b6d213e59866e62236ad975d32d54d88e1e4
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-13 13:02:10 +0000

    start

---
+++ project/description.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.markdown("""
+### Pós-graduação em Ciência da Computação - UEL
+**Projeto:** Detecção de Ataques no Tráfego de Dispositivos IoT.
+            
+**Disciplina:** 2COP507 Reconhecimento de Padrões.
+                               
+**Discente:** Wesley Candido da Silva.
+            
+**Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
+
+**Link dataset:** https://www.kaggle.com/datasets/mkashifn/nbaiot-dataset
+            
+**Link github:** xxx
+            
+**Link colab:** xx
+""")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,14 @@
 import streamlit as st
+import streamlit as st
+import os
 
+# Read files *.py
+file_path = os.path.join("code", "description.py")
+with open(file_path) as f:
+    exec(f.read())
+
+# Read files *.py
+file_path = os.path.join("code", "model_ml.py")
+with open(file_path) as f:
+    exec(f.read())
 
