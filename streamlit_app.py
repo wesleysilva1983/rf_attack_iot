5aebab90b8eb7f0ef9b327a6c6a4359ca146eda9
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-13 13:03:08 +0000

    start

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import streamlit as st
 import os
 
 # Read files *.py
-file_path = os.path.join("code", "description.py")
+file_path = os.path.join("project", "description.py")
 with open(file_path) as f:
     exec(f.read())
 
 # Read files *.py
-file_path = os.path.join("code", "model_ml.py")
+file_path = os.path.join("project", "model_ml.py")
 with open(file_path) as f:
     exec(f.read())
 
