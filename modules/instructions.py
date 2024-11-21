9a9553949f17a8aa7009a2d95b2a085c7a70f059
Wesley Silva <56238015+wesleysilva1983@users.noreply.github.com>
2024-11-21 02:01:36 +0000

    rf_vfinal

--- modules/instructions.py
+++ modules/instructions.py
@@ -3,15 +3,16 @@ import streamlit as st
 def show():
     st.markdown("""
     ### Pós-graduação em Ciência da Computação - UEL
-    **Projeto 2COP507:** Detecção de Ataques no Tráfego de Dispositivos IoT.
+    **Projeto 2COP507:** Detecção de Ataques no Tráfego de Dispositivos IoT .
                                    
     **Discente:** Wesley Candido da Silva.
             
-    **Dataset:** N-BaIoT Dataset to Detect IoT Botnet Attacks
-                
-    **Link dataset:**  https://www.kaggle.com/datasets/mkashifn/nbaiot-dataset
+    **Dataset:**    N-BaIoT Dataset to Detect IoT Botnet Attacks
+                    https://www.kaggle.com/datasets/mkashifn/nbaiot-dataset
           
-    **Link github:** https://github.com/wesleysilva1983/rf_attack_iot
+    **GitHub:** https://github.com/wesleysilva1983/rf_attack_iot
+                
+    **YouTube:** https://youtu.be/rfkwhLMyxOM
     """)
 
     st.markdown("""
